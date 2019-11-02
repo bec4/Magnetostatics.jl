bb4f6f2e6b7615d219d317fb9e8f7b48bb380746
Julius de Hond <dehond@mit.edu>
2019-11-02 17:46:57 -0400

    Add Julia package metadata file.

--- src/Magnetostatics.jl
+++ src/Magnetostatics.jl
@@ -1,7 +1,5 @@
 module Magnetostatics
 
-using ForwardDiff
-
 export normalCoil, coilStack
 export pathVec, normCubed, crossArray, biasField, fieldNorm, fieldZComponent, fieldNormplusBias
 include("coils.jl")
