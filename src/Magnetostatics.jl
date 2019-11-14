f7b354a67381ed25cbe82499e9ed555f42cffec2
Jinggang Xiang <jinggang@mit.edu>
2019-11-14 12:33:01 -0500

    export new defined functions

--- src/Magnetostatics.jl
+++ src/Magnetostatics.jl
@@ -1,7 +1,8 @@
 module Magnetostatics
 
 export normalCoil, coilStack
-export pathVec, normCubed, crossArray, biasField, fieldNorm, fieldZComponent, fieldNormplusBias
+export pathVec, normCubed, crossArray, biasField, fieldNorm, fieldZComponent,fieldXComponent,fieldYComponent, fieldNormplusBias
+
 include("coils.jl")
 include("biotSavartCalculus.jl")
 
